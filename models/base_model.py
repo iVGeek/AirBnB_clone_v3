1d6337f4ebee893dbafcf982c3a1669fa950974a
iVGeek <ianvincent916@gmail.com>
2023-04-30 10:50:33 -0700

    recode

--- models/base_model.py
+++ models/base_model.py
@@ -2,7 +2,7 @@
 """
 Contains class BaseModel
 """
-
+import inspect
 from datetime import datetime
 import models
 from os import getenv
@@ -10,7 +10,6 @@ import sqlalchemy
 from sqlalchemy import Column, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 import uuid
-import hashlib
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 
@@ -69,9 +68,14 @@ class BaseModel:
         new_dict["__class__"] = self.__class__.__name__
         if "_sa_instance_state" in new_dict:
             del new_dict["_sa_instance_state"]
-        if models.storage_t == 'db':
-            if 'password' in new_dict:
-                del new_dict['password']
+        frame = inspect.currentframe().f_back
+        func_name = frame.f_code.co_name
+        class_name = ''
+        if 'self' in frame.f_locals:
+            class_name = frame.f_locals["self"].__class__.__name__
+        is_fs_writing = func_name == 'save' and class_name == 'FileStorage'
+        if 'password' in new_dict and not is_fs_writing:
+            del new_dict['password']
         return new_dict
 
     def delete(self):
